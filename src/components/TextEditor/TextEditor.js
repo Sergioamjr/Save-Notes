d9e15e144ecd2fcb748c78ad656a1d34eeba3cb9
Sergioamjr <sergioamjr91@gmail.com>
2019-07-27 14:36:46 +0100

    Catching throw error to avoid crash

--- src/components/TextEditor/TextEditor.js
+++ src/components/TextEditor/TextEditor.js
@@ -3,19 +3,20 @@ import Editor from "@stfy/react-editor.js";
 import { SetSwitchAsTrue } from "../../store/notes/index";
 import { connect } from "react-redux";
 
-const TextEditor = props => {
-  return (
-    <Editor
-      onReady={() => {
-        setTimeout(() => {
-          props.dispatch(SetSwitchAsTrue());
-        }, 500);
-      }}
-      autofocus
-      holderId="editorjs-wrapper"
-      {...props}
-    />
-  );
-};
+class TextEditor extends React.Component {
+  componentDidCatch = () => {};
+  render() {
+    return (
+      <Editor
+        onReady={() => {
+          this.props.dispatch(SetSwitchAsTrue());
+        }}
+        autofocus
+        holderId="editorjs-wrapper"
+        {...this.props}
+      />
+    );
+  }
+}
 
 export default connect()(TextEditor);
