0a2e0fec7699e8a41d9859f1ae94b5c5afd297b5
Sergioamjr <sergioamjr91@gmail.com>
2019-07-27 15:00:02 +0100

    Add button to create a new doc in sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,47 @@
 import React from "react";
 import NotesItem from "../NotesItem";
 import _get from "lodash/get";
+import Button from "../Button";
+import { AddNoteInList } from "../../store/notes/index";
+import { history } from "../../routes/Routes";
+
+const NoteFactoty = dateTime => {
+  return {
+    titulo: `${dateTime}`,
+    data: {
+      time: dateTime,
+      blocks: [
+        {
+          type: "header",
+          data: {
+            text: `text ${dateTime}`,
+            level: 1
+          }
+        }
+      ]
+    },
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et sollicitudin sem, sit amet semper neque. Aliquam mi dui, ultrices sit amet leo in, vulputate dignissim lorem",
+    date: dateTime,
+    id: dateTime
+  };
+};
 
 const Sidebar = props => {
   const list = _get(props, "notes.list", []);
   const canSwitchNote = _get(props, "notes.canSwitchNote");
+
+  const addNoteInListHandler = () => {
+    const dateTime = new Date().getTime();
+    const data = NoteFactoty(dateTime);
+    props.dispatch(AddNoteInList(data));
+    history.navigate(`/documento/${dateTime}`);
+  };
   return (
     <aside className="background-theme  ">
       <h1 className="p-10 color-white fs-5 m-bottom-10 m-top-20">MyNotes</h1>
       <div>
+        <Button onClick={addNoteInListHandler}>Novo Documento</Button>
         {list.length === 0 && (
           <p className="color-white p-10">Nenhum documento criado ainda.</p>
         )}
--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -1,29 +1,7 @@
 import React from "react";
 import Button from "../Button/Button";
-import { RemoveNoteFromList, AddNoteInList } from "../../store/notes/index";
+import { RemoveNoteFromList } from "../../store/notes/index";
 import { history } from "../../routes/Routes";
-
-const NoteFactoty = dateTime => {
-  return {
-    titulo: `${dateTime}`,
-    data: {
-      time: dateTime,
-      blocks: [
-        {
-          type: "header",
-          data: {
-            text: `text ${dateTime}`,
-            level: 1
-          }
-        }
-      ]
-    },
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et sollicitudin sem, sit amet semper neque. Aliquam mi dui, ultrices sit amet leo in, vulputate dignissim lorem",
-    date: dateTime,
-    id: dateTime
-  };
-};
 
 const TopMenu = props => {
   const removeItemHandler = () => {
@@ -34,18 +12,9 @@ const TopMenu = props => {
     history.navigate("/");
   };
 
-  const addNoteInListHandler = () => {
-    const dateTime = new Date().getTime();
-    const data = NoteFactoty(dateTime);
-    props.dispatch(AddNoteInList(data));
-    history.navigate(`/documento/${dateTime}`);
-  };
-
   return (
     <div className="d-flex d-flex-space-between d-flex-align-center m-bottom-40">
       <div>
-        <Button onClick={addNoteInListHandler}>Novo Documento</Button>
-
         {props.hasFoundNote && (
           <Button onClick={removeItemHandler}>Excluir Documento</Button>
         )}
