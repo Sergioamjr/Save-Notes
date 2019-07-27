d759b2fb9da96514e4920c3627cb4b498b87dd9c
Sergioamjr <sergioamjr91@gmail.com>
2019-07-27 11:28:22 +0100

    Fixed tdd crash

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import { shallow } from "enzyme";
+import App from "./routes";
+const mockStore = configureMockStore();
+const store = mockStore({});
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  shallow(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
